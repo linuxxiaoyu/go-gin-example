5708556f091e15a308c1117722c4b1eb863977e3
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-09-07 16:06:49 +0800

    Update pkg app

---
+++ pkg/app/request.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"github.com/astaxie/beego/validation"
+	"github.com/linuxxiaoyu/go-gin-example/pkg/logging"
+)
+
+func MarkErrors(errors []*validation.Error) {
+	for _, err := range errors {
+		logging.Info(err.Key, err.Message)
+	}
+
+	return
+}
