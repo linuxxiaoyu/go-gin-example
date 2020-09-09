f1784e28d6d9a4c2dcf3ba166c7434332484d4e6
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-09-09 09:00:09 +0800

    Add pkg export

---
+++ pkg/export/excel.go
@@ -0,0 +1,17 @@
+package export
+
+import (
+	"github.com/linuxxiaoyu/go-gin-example/pkg/setting"
+)
+
+func GetExcelFullUrl(name string) string {
+	return setting.AppSetting.PrefixUrl + "/" + GetExcelPath() + name
+}
+
+func GetExcelPath() string {
+	return setting.AppSetting.ExportSavePath
+}
+
+func GetExcelFullPath() string {
+	return setting.AppSetting.RuntimeRootPath + GetExcelPath()
+}
